87e135f9c589441fa83a327d073e676f52f429d5
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-07-08 18:49:55 +0800

    feat(management): align with new linting stuff

--- src/endstone_twitch_spawn/actions/management.py
+++ src/endstone_twitch_spawn/actions/management.py
@@ -1,15 +1,19 @@
 from endstone import Logger
 from pathlib import Path
-from .models import Workflow
-from .parser import parse_workflow_file
+from .models import Workflow, FailedWorkflow
+from .parser import parse_workflow
 
 
 class WorkflowManager:
+    workflows: list[Workflow]
+    failed_workflows: list[FailedWorkflow]
+
     def __init__(self, folder: Path, logger: Logger):
         self.folder = folder
         self.folder.mkdir(parents=True, exist_ok=True)
         self._logger = logger
         self.workflows: list[Workflow] = []
+        self.failed_workflows: list[FailedWorkflow] = []
 
     def scan_for_workflows(self):
         workflow_files = [
@@ -19,8 +23,15 @@ class WorkflowManager:
         ]
 
         self.workflows.clear()
+        self.failed_workflows.clear()
+        
         for file in workflow_files:
             try:
-                self.workflows.append(parse_workflow_file(file))
+                parsed_workflow = parse_workflow(file)
+                match parsed_workflow:
+                    case Workflow():
+                        self.workflows.append(parsed_workflow)
+                    case FailedWorkflow():
+                        self.failed_workflows.append(parsed_workflow)
             except Exception as e:
                 self._logger.error(f"Error while parsing workflow {file.name}:\n{e}")
