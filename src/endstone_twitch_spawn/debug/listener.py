6d826eb713dc6cbc2d3c781d59c396bc9f1519af
MakiTazo <laalberto96@gmail.com>
2026-07-14 15:44:43 -0600

    removing old twitchio feats

--- src/endstone_twitch_spawn/debug/listener.py
+++ src/endstone_twitch_spawn/debug/listener.py
@@ -1,6 +1,6 @@
 from ..events.base import StreamEvent, stream_event_handler
 from ..events.streamlabs import EVENTS as STREAMLABS_EVENTS
-from ..events.twitchio import EVENTS as TWITCHIO_EVENTS
+from ..events.twitchapi import EVENTS as TWITCHAPI_EVENTS
 from endstone import Logger
 
 
@@ -23,7 +23,7 @@ def _bind_events(event_types: list):
     return class_decorator
 
 
-@_bind_events(STREAMLABS_EVENTS + TWITCHIO_EVENTS)
+@_bind_events(STREAMLABS_EVENTS + TWITCHAPI_EVENTS)
 class Listener:
     def __init__(
         self,
