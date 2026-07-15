12e6db01b4b90c9037a48142ae43819030bce7f8
MakiTazo <laalberto96@gmail.com>
2026-07-15 10:59:05 -0600

    add: twitchapi events

--- src/endstone_twitch_spawn/events/__init__.py
+++ src/endstone_twitch_spawn/events/__init__.py
@@ -1,5 +1,6 @@
 from .streamlabs import EVENTS as STREAMLABS_EVENTS
+from .twitchapi import EVENTS as TWITCHAPI_EVENTS
 from .base import StreamEvent
 from typing import Sequence, Type
 
-ALL_EVENTS: Sequence[Type[StreamEvent]] = STREAMLABS_EVENTS
+ALL_EVENTS: Sequence[Type[StreamEvent]] = STREAMLABS_EVENTS + TWITCHAPI_EVENTS
