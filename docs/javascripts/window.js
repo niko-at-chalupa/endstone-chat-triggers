2a470077b486d42952014230d8edb68b48efb4bd
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-07-17 13:24:15 +0800

    docs: add font files and update styles and scripts for enhanced typography and animations

---
+++ docs/javascripts/window.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (typeof gsap !== "undefined" && typeof ScrollTrigger !== "undefined") {
+    gsap.registerPlugin(ScrollTrigger);
+
+    ScrollTrigger.batch("blockquote", {
+      start: "top 85%",
+      onEnter: (batch) => {
+        gsap.to(batch, {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          ease: "power4.out",
+          stagger: {
+            amount: 0.05,
+            grid: "auto",
+            from: "start"
+          }
+        });
+      },
+      once: true
+    });
+  }
+});
